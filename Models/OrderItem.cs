695e9a2e744e9c365472d88ae9fcfc0379139d6f
Denis Solovyov <ya@den-sol-kzn.ru>
2025-09-27 11:10:00 +0300

    feat(OrderItem): add GetTotalPrice methor and ToString method override

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -10,4 +10,11 @@ public class OrderItem
         Product = product ?? throw new ArgumentNullException(nameof(product));
         Quantity = quantity;
     }
+
+    public decimal GetTotalPrice()
+    {
+        return Product.Price * Quantity;
+    }
+
+    public override string ToString() => $"{Product.Name} * {Quantity} = {GetTotalPrice():N2} ₽";
 }
